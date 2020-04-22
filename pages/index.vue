bfc436aec0aebcc49890049eb7f16e46b02bc05e
David Royer <droyer01@gmail.com>
2020-04-22 09:22:35 -0400

    chore: testing

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,12 @@
 
 <script>
 export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    };
+  },
+
   computed: {
     blogPosts() {
       return this.$store.state.blogPosts;
